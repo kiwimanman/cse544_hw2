5919a7550a81504480046cf1985f0ffd8b9ca7cc
kiwimanman <kiwimanman@gmail.com>
2014-01-26 19:51:07 -0800

    Create  hw2-q2.sql

---
+++ hw2-q2.sql
@@ -0,0 +1,51 @@
+CREATE TABLE InsuranceCo (
+  name  TEXT PRIMARY KEY,
+  phone TEXT
+);
+
+CREATE TABLE Person (
+  ssn  INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE  Vehicle (
+  licencePlate  TEXT PRIMARY KEY,
+  year          INTEGER,
+  insured_by    TEXT REFERENCES InsuranceCo(name),
+  maxLossDamage INTEGER,
+  maxLiability  INTEGER,
+  owned_by      INTEGER REFERENCES Person(ssn)
+);
+
+CREATE TABLE Driver (
+  licenceNo TEXT PRIMARY KEY,
+  person_id REFERENCES Person(ssn)
+);
+
+CREATE TABLE nonProfessionalDriver (
+  licenceNo      TEXT PRIMARY KEY REFERENCES Driver(licenceNo)
+);
+
+CREATE TABLE ProfessionalDriver (
+  licenceNo      TEXT PRIMARY KEY REFERENCES Driver(licenceNo),
+  medicalHistroy TEXT
+);
+
+CREATE TABLE Car (
+  licencePlate PRIMARY KEY REFERENCES Vehicle(licencePLate),
+  make         TEXT
+);
+
+CREATE TABLE Truck (
+  licencePlate PRIMARY KEY REFERENCES Vehicle(licencePLate),
+  capacity     INTEGER,
+  operated_by  TEXT REFERENCES ProfessionalDriver(licenceNo)
+);
+
+CREATE TABLE Cars_nonProfessionalDrivers (
+  id           INTEGER PRIMARY KEY,
+  licencePLate TEXT REFERENCES Car(licencePLate), 
+  licenceNo    TEXT REFERENCES nonProfessionalDriver(licenceNo) 
+);
+
+
